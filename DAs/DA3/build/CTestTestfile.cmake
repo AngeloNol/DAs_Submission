2bbfdac2b2391a055f2bdf6eebe1f9ebb5288c5b
Angelo <98061732+AngeloNol@users.noreply.github.com>
2023-12-07 12:42:38 -0800

    Add files via upload

---
+++ DAs/DA3/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/angelo/CpE476/DA3/src
+# Build directory: /home/angelo/CpE476/DA3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
